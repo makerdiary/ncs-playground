fc07044dfa829b92f55ddf52b57d216badd2afcb
Zelin Cai <zelin@makerdiary.com>
2023-02-23 11:32:26 +0800

    samples: button: Use USB CDC ACM as backend for Console.
    Signed-off-by: Zelin Cai <zelin@makerdiary.com>

--- samples/button/src/main.c
+++ samples/button/src/main.c
@@ -11,12 +11,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/usb/usb_device.h>
-#include <zephyr/drivers/uart.h>
 #include <inttypes.h>
-
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-         "Console device is not ACM CDC UART device");
 
 #define SLEEP_TIME_MS    1
 
@@ -47,19 +42,6 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb,
 void main(void)
 {
     int ret;
-    const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-    uint32_t dtr = 0;
-
-    if (usb_enable(NULL)) {
-        return;
-    }
-
-    /* Poll if the DTR flag was set */
-    while (!dtr) {
-        uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-        /* Give CPU resources to low priority threads. */
-        k_sleep(K_MSEC(100));
-    }
 
     if (!device_is_ready(button.port)) {
         printk("Error: button device %s is not ready\n",
