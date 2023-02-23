bd06a01a9b79939ad6f37c11b55be4bc1273e800
Zelin Cai <zelin@makerdiary.com>
2023-02-23 12:20:26 +0800

    smaples: ble: observer: Use USB CDC ACM as backend for Console.
    Signed-off-by: Zelin Cai <zelin@makerdiary.com>

--- samples/ble/observer/src/main.c
+++ samples/ble/observer/src/main.c
@@ -7,31 +7,12 @@
 
 #include <zephyr/sys/printk.h>
 #include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/usb/usb_device.h>
-#include <zephyr/drivers/uart.h>
-
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-         "Console device is not ACM CDC UART device");
 
 int observer_start(void);
 
 void main(void)
 {
     int err;
-
-    const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-    uint32_t dtr = 0;
-
-    if (usb_enable(NULL)) {
-        return;
-    }
-
-    /* Poll if the DTR flag was set */
-    while (!dtr) {
-        uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-        /* Give CPU resources to low priority threads. */
-        k_sleep(K_MSEC(100));
-    }
 
     printk("Starting Observer Demo\n");
 
